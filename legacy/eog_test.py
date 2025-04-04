e8f6cc5871e3485aa5d6999957790f5070145a20
Akhil Ganesan <akhilganesan526@gmail.com>
2025-04-04 18:30:14 -0400

    Poor Blink Detection

--- legacy/eog_test.py
+++ legacy/eog_test.py
@@ -5,11 +5,11 @@ import keyboard
 from collections import deque
 
 # Set up the serial port and parameters
-serial_port = 'COM7'  # Replace with your Arduino's serial port (e.g., '/dev/ttyUSB0' on Linux or 'COM3' on Windows)
+serial_port = 'COM6'  # Replace with your Arduino's serial port (e.g., '/dev/ttyUSB0' on Linux or 'COM3' on Windows)
 baud_rate = 230400
 ser = serial.Serial(serial_port, baud_rate)
-history = 200
-update_rate = 1
+history = 500
+update_rate = 10
 
 # Setup the plot
 plt.ion()  # Turn on interactive mode
@@ -18,7 +18,7 @@ x_data = deque(maxlen=history)  # Store the last 100 data points
 y_data = deque(maxlen=history)
 line, = ax.plot([], [], 'r-')  # Red line
 ax.set_xlim(0, history)
-ax.set_ylim(0, 1000000)  # Assuming the sensor reads between 0 and 1023
+ax.set_ylim(0, 1000)  # Assuming the sensor reads between 0 and 1023
 
 # for i in range(200):
 #     x_data.append(i)
@@ -40,25 +40,31 @@ update = history // update_rate
 while True:
     if ser.in_waiting > 0:
         try:
-            data = ser.readline().decode('utf-8').strip().replace(',','')  # Read and decode data from serial
-            print(int(data))
+            data = ser.readline().decode('utf-8').strip().split(',')[0]  # Read and decode data from serial
+            # print(int(data))
             # data = float(data)
             # x_data.append(data)
             # print(ser.in_waiting)
-            if data.isdigit():  # Ensure the data is numeric
-                value = int(data)
-                # print(value)
-                x_data.append(len(x_data))
-                y_data.append(value)
-                readings += 1
+            # if data.isdigit():  # Ensure the data is numeric
+            value = int(data)
+            # print(value)
+            x_data.append(len(x_data))
+            y_data.append(value)
+            readings += 1
+            if readings > 2:
+                # print("Check", y_data[-2], y_data[-1])
+                if y_data[-1] < 480 <= y_data[-2]:
+                    print("Blink")
             if readings > update:
                 update_plot()
                 readings = 0
-                plt.pause(1)
+                plt.pause(0.01)
             # if (y_data[-1] < 600):
             #     pass
-                # keyboard.press('space')
-                # keyboard.release('space')
+            #     keyboard.press('space')
+            #     keyboard.release('space')
+        except ValueError:
+            pass
         except UnicodeDecodeError:
             pass
     if keyboard.is_pressed('q'):
