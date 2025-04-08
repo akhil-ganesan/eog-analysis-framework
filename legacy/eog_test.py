fb5a23fb91d4000b8a9c65256882344ade59a46f
Akhil Ganesan <akhilganesan526@gmail.com>
2025-04-08 18:48:32 -0400

    Blink worked and now doesn't work

--- legacy/eog_test.py
+++ legacy/eog_test.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import keyboard
 from collections import deque
+import neurokit
 
 # Set up the serial port and parameters
 serial_port = 'COM6'  # Replace with your Arduino's serial port (e.g., '/dev/ttyUSB0' on Linux or 'COM3' on Windows)
@@ -10,25 +11,26 @@ baud_rate = 230400
 ser = serial.Serial(serial_port, baud_rate)
 history = 500
 update_rate = 10
+a = 0.5
 
 # Setup the plot
-plt.ion()  # Turn on interactive mode
-fig, ax = plt.subplots()
+# plt.ion()  # Turn on interactive mode
+# fig, ax = plt.subplots()
 x_data = deque(maxlen=history)  # Store the last 100 data points
 y_data = deque(maxlen=history)
-line, = ax.plot([], [], 'r-')  # Red line
-ax.set_xlim(0, history)
-ax.set_ylim(0, 1000)  # Assuming the sensor reads between 0 and 1023
+# line, = ax.plot([], [], 'r-')  # Red line
+# ax.set_xlim(0, history)
+# ax.set_ylim(0, 1000)  # Assuming the sensor reads between 0 and 1023
 
 # for i in range(200):
 #     x_data.append(i)
 #     y_data.append(i)
 
 # Function to update the plot
-def update_plot():
-    line.set_xdata(np.arange(len(x_data)))  # X-axis
-    line.set_ydata(np.array(y_data))  # Y-axis
-    plt.draw()
+# def update_plot():
+#     line.set_xdata(np.arange(len(x_data)))  # X-axis
+#     line.set_ydata(np.array(y_data))  # Y-axis
+#     plt.draw()
 
 
 # Read and plot data in real-time
@@ -47,18 +49,23 @@ while True:
             # print(ser.in_waiting)
             # if data.isdigit():  # Ensure the data is numeric
             value = int(data)
+            # if y_data:
+            #     value = value * a + (1 - a) * y_data[-1]
             # print(value)
             x_data.append(len(x_data))
             y_data.append(value)
             readings += 1
             if readings > 2:
                 # print("Check", y_data[-2], y_data[-1])
-                if y_data[-1] < 480 <= y_data[-2]:
+                if y_data[-1] < 475 <= y_data[-2]:
                     print("Blink")
+                    keyboard.press('space')
+                else:
+                    keyboard.release('space')
             if readings > update:
-                update_plot()
+                # update_plot()
                 readings = 0
-                plt.pause(0.01)
+                # plt.pause(0.01)
             # if (y_data[-1] < 600):
             #     pass
             #     keyboard.press('space')
@@ -71,6 +78,6 @@ while True:
             # keyboard.release('space')
             break
 
-ser.close()
-plt.ioff()  # Turn off interactive mode
-plt.show()  # Keep the plot open after program finishes
+# ser.close()
+# plt.ioff()  # Turn off interactive mode
+# plt.show()  # Keep the plot open after program finishes
